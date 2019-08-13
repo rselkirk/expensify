98ffde9f91ef4845101074231010b3e3da566a30
Rachel Selkirk <rachel.selkirk@gmail.com>
2019-08-13 12:02:45 -0700

    adjust edit route

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    This is from my edit expense component
-  </div>
-);
+const EditExpensePage = (props) => {
+  return (
+    <div>
+      Editing expense with id of {props.match.params.id}
+    </div>
+    );
+  };
 
 export default EditExpensePage;
