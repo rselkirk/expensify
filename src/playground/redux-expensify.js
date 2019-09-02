547dc0a34797f169138eeea2598f3b1ce3970761
Rachel Selkirk <rachel.selkirk@gmail.com>
2019-09-02 10:38:24 -0700

    added sorting and filtering

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -130,9 +130,6 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
   }
 };
 
-// timestamps
-//convert to lower case, check includes
-
 // Get visible expenses
 const getVisibleExpense = (expenses, { text, sortBy, startDate, endDate }) => {
   return expenses.filter((expense) => {
@@ -141,6 +138,12 @@ const getVisibleExpense = (expenses, { text, sortBy, startDate, endDate }) => {
     const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
     return startDateMatch && endDateMatch && textMatch;
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1;
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1
+    }
   });
 };
 
@@ -159,13 +162,13 @@ store.subscribe(() => {
   console.log(visibleExpenses);
 });
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: 1000 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt: -21000 }));
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }));
 
 // store.dispatch(removeExpense({id: expenseOne.expense.id }));
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 
-store.dispatch(setTextFilter('rent'));
+// store.dispatch(setTextFilter('rent'));
 // store.dispatch(setTextFilter());
 
 // store.dispatch(sortByAmount());
