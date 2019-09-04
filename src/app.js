be47e0222e40f7c99e0ef23e355957c760ce47f2
Rachel Selkirk <rachel.selkirk@gmail.com>
2019-09-04 15:43:06 -0700

    connect expenses component

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -14,11 +15,18 @@ store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: 2
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 50, createdAt: 2100 }));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000)
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpense(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
 
-console.log(store.getState());
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx , document.getElementById('app'));
