ecef749e6796d9dba36387ab592e98544ea7a69d
Rachel Selkirk <rachel.selkirk@gmail.com>
2019-09-11 12:08:10 -0700

    form is submitting

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ const store = configureStore();
 store.dispatch(addExpense({ description: 'Water Bill', amount: 4100, createdAt: 2000 }));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 1000, createdAt: 2100 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 109000, createdAt: 2100 }));
-store.dispatch(setTextFilter('water'));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpense(state.expenses, state.filters);
--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -10,7 +10,8 @@ export default class ExpenseForm extends React.Component {
     note: '',
     amount: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   };
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -18,12 +19,14 @@ export default class ExpenseForm extends React.Component {
   };
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   }
   onDateChange = (createdAt) => {
-    this.setState(() => ({ createdAt }));
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
   }
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }));
@@ -32,10 +35,26 @@ export default class ExpenseForm extends React.Component {
     const note = e.target.value;
     this.setState(() => ({ note }));
   };
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide description and amount.'}));
+    } else {
+      this.setState(() => ({ error: '' }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      });
+    }
+  }
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input
             type='text'
             placeholder='Description'
