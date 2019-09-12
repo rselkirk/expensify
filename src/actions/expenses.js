4f646884a2c10d85719c53c7b53765a7cb67d799
Rachel Selkirk <rachel.selkirk@gmail.com>
2019-09-12 11:09:38 -0700

    moved remove button, wired up expense edit

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -25,7 +25,7 @@ export const removeExpense = ({ id } = {}) => ({
 });
 
 // EDIT_EXPENSE
-export const editExpense = ({ id }, updates) => ({
+export const editExpense = (id, updates) => ({
   type: 'EDIT_EXPENSE',
   id,
   updates
