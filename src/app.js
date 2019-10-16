17aca5230874826751e3354b3662e698784263d5
Rachel Selkirk <rachel.selkirk@gmail.com>
2019-10-16 07:05:38 -0700

    set up production webpack

--- src/app.js
+++ src/app.js
@@ -10,16 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 const store = configureStore();
-
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4100, createdAt: 2000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 1000, createdAt: 2100 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109000, createdAt: 2100 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpense(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
